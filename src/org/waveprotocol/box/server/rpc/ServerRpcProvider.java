9aab5378fc90ad71664fca976a5fde45f74ce861
Yuri Zelikov <yurize@apache.org>
2011-11-26 19:42:15 +0000

    Returns disabling of JSessionIDs in URLs redirects
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wave/trunk@1206559 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/waveprotocol/box/server/rpc/ServerRpcProvider.java
+++ src/org/waveprotocol/box/server/rpc/ServerRpcProvider.java
@@ -349,6 +349,11 @@ public class ServerRpcProvider {
     context.setParentLoaderPriority(true);
 
     if (jettySessionManager != null) {
+      // This disables JSessionIDs in URLs redirects
+      // see: http://stackoverflow.com/questions/7727534/how-do-you-disable-jsessionid-for-jetty-running-with-the-eclipse-jetty-maven-plu
+      // and: http://jira.codehaus.org/browse/JETTY-467?focusedCommentId=114884&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-114884
+      jettySessionManager.setSessionIdPathParameterName(null);
+      
       context.getSessionHandler().setSessionManager(jettySessionManager);
     }
     final ResourceCollection resources = new ResourceCollection(resourceBases);
