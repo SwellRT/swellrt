b6f6f5059ccc7e291ec8ac9366975dc494b1f217
Ali Lown <al@apache.org>
2013-08-27 19:20:37 +0000

    "Licensed under Apache" -> "Licensed to the ASF"
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wave/branches/wave-0.4-release@1517932 13f79535-47bb-0310-9956-ffa450edef68

--- src/python/api/commandline_robot_runner.py
+++ src/python/api/commandline_robot_runner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2.4
 #
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -17,10 +16,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-#
-#
-#
-# Copyright 2009 Google Inc. All Rights Reserved.
 
 """Run robot from the commandline for testing.
 
--- src/python/api/testdata.py
+++ src/python/api/testdata.py
@@ -14,8 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-#
-#
+
+
 json_string = """{
     "wavelet": {
         "creationTime": 1277873815463,
