ef9a9a93e6de8c169e0510f454c10ad5828eee1b
Yuri Zelikov <yurize@apache.org>
2014-08-21 21:29:44 +0300

    WAVE-311 Allows to download the Solr dependency via ant and run with run-solr.sh script.

---
+++ run-solr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# This script will start the Solr server.
+
+# Make sure the third_party/solr/solr-4.9.0/example folder exists.
+if [ ! -d third_party/solr/solr-4.9.0/example ]; then
+  echo "You need to download the solr third party dependency into third_party/solr/ and unzip it by running: ant get-third-party-solr-dep. "
+  echo "Or download it manually from http://apache.spd.co.il/lucene/solr/4.9.0/ into third_party/solr and unzip there."
+  exit 1
+fi
+
+
+exec java \
+  -Dsolr.solr.home=third_party/solr/solr-4.9.0/example/solr\
+  -Djetty.home=third_party/solr/solr-4.9.0/example\
+  -jar third_party/solr/solr-4.9.0/example/start.jar
