f77a6a94642ce019aa30009debf6c76cb3575989
pablojan <pablojan@pablojan@gmail.com>
2014-10-28 12:24:44 +0100

    Fixed bugs in DocBased Project and Tasks

---
+++ src/org/waveprotocol/mod/wavejs/js/p2pvalue/TaskJS.java
@@ -0,0 +1,78 @@
+package org.waveprotocol.mod.wavejs.js.p2pvalue;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+import org.waveprotocol.mod.model.p2pvalue.Task;
+import org.waveprotocol.wave.model.wave.ParticipantId;
+
+/**
+ * A JavaScript wrapper for the Task class
+ *
+ * @author pablojan@gmail.com
+ *
+ */
+public class TaskJS extends JavaScriptObject implements Task.Listener {
+
+      public native static TaskJS create(Task delegate) /*-{
+
+        var jso = {
+
+         _delegate: delegate,
+
+         callbackMap: new Object(),
+
+         eventHandlers: new Object(),
+
+         registerEventHandler: function(event, handler) {
+          this.eventHandlers[event] = handler;
+         },
+
+         unregisterEventHandler: function(event, handler) {
+          this.eventHandlers[event] = null;
+         }
+
+       }; // jso
+
+    return jso;
+
+  }-*/;
+
+
+  protected TaskJS() {
+
+  }
+
+  private final native void fireEvent(String event, Object parameter) /*-{
+
+    if (this.eventHandlers[event] != null) {
+      this.eventHandlers[event](parameter);
+    }
+
+  }-*/;
+
+
+  @Override
+  public final void onStatusChanged(String status) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public final void onDeadlineChanged(long deadline) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public final void onParticipantAdded(ParticipantId participant) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public final void onParticipantRemoved(ParticipantId participant) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
