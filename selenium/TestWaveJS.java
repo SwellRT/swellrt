d575a941e229a170b76a4a5fe6aa8f0a9e9870d8
Antonio Tapiador del Dujo <atapiador@ucm.es>
2014-11-03 14:43:24 +0100

    Check jasmine suite in selenium tests

--- selenium/TestWaveJS.java
+++ selenium/TestWaveJS.java
@@ -1,5 +1,3 @@
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,32 +13,45 @@ public class TestWaveJS  {
     WebDriver driver = new ChromeDriver();
 
     // And now use this to visit Google
-    driver.get("http://www.google.com");
+    driver.get("http://localhost:9898/test/wavejs.html");
     // Alternatively the same thing can be done like this
     // driver.navigate().to("http://www.google.com");
 
     // Find the text input element by its name
-    WebElement element = driver.findElement(By.name("q"));
+    WebElement uid = driver.findElement(By.id("uid"));
 
     // Enter something to search for
-    element.sendKeys("Cheese!");
+    uid.sendKeys("test");
+
+    WebElement pwd = driver.findElement(By.id("password"));
+
+    pwd.sendKeys("test");
 
     // Now submit the form. WebDriver will find the form for us from the element
-    element.submit();
+    uid.submit();
 
     // Check the title of the page
-    System.out.println("Page title is: " + driver.getTitle());
+    //System.out.println("Page title is: " + driver.getTitle());
 
-    // Google's search is rendered dynamically with JavaScript.
+    // login
+    driver.findElement(By.id("login")).click();
+
     // Wait for the page to load, timeout after 10 seconds
     (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
       public Boolean apply(WebDriver d) {
-        return d.getTitle().toLowerCase().startsWith("cheese!");
+        return d.findElement(By.id("logoutForm")).isDisplayed();
       }
     });
 
-    // Should see: "cheese! - Google Search"
-    System.out.println("Page title is: " + driver.getTitle());
+    // run test suite
+    driver.findElement(By.id("runP2PvalueTest")).click();
+
+    // Wait for the page to load, timeout after 10 seconds
+    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+      public Boolean apply(WebDriver d) {
+        return d.findElement(By.cssSelector("span.bar.passed")).isDisplayed();
+      }
+    });
 
     // Close the browser
     driver.quit();
