f632d19ad4653c266b25de654db6a5b18f2aca79
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2014-11-05 17:51:07 +0100

    Test login with domain in user ID field

--- selenium/TestWaveJS.java
+++ selenium/TestWaveJS.java
@@ -14,44 +14,44 @@ public class TestWaveJS  {
       // Notice that the remainder of the code relies on the interface,
       // not the implementation.
       WebDriver driver = new ChromeDriver();
-  
+
       (new WebDriverWait(driver, 180)).until(new ExpectedCondition<Boolean>() {
         public Boolean apply(WebDriver d) {
           d.get("http://localhost:9898/test/wavejs.html");
-  
+
           return d.findElement(By.id("loginForm")).isDisplayed();
         }
       });
-  
+
       // Find the text input element by its name
       WebElement uid = driver.findElement(By.id("uid"));
-  
+
       // Enter something to search for
-      uid.sendKeys("test");
-  
+      uid.sendKeys("test@local.net");
+
       WebElement pwd = driver.findElement(By.id("password"));
-  
+
       pwd.sendKeys("test");
-  
+
       // Now submit the form. WebDriver will find the form for us from the element
       uid.submit();
-  
+
       // Check the title of the page
       //System.out.println("Page title is: " + driver.getTitle());
-  
+
       // login
       driver.findElement(By.id("login")).click();
-  
+
       // Wait for the page to load, timeout after 10 seconds
       (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
         public Boolean apply(WebDriver d) {
           return d.findElement(By.id("logoutForm")).isDisplayed();
         }
       });
-  
+
       // run test suite
       driver.findElement(By.id("runP2PvalueTest")).click();
-  
+
       // Wait for the page to load, timeout after 10 seconds
       (new WebDriverWait(driver, 60)).until(new ExpectedCondition<Boolean>() {
         public Boolean apply(WebDriver d) {
