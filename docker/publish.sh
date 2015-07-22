658c8fa020975e5a348cd18819691371965dc2a3
Antonio Tapiador del Dujo <atapiador@ucm.es>
2015-07-22 13:38:45 +0200

    Wait for git tag for docker tagging

--- docker/publish.sh
+++ docker/publish.sh
@@ -2,13 +2,13 @@
 
 NAME=p2pvalue/swellrt
 
-echo "Building image"
+echo "Building image.."
 
 docker build -t $NAME .
 
 if [ $? -ne 0 ]; then exit 1; fi
 
-echo "Publishing image"
+echo "Publishing image.."
 
 docker push $NAME
 
@@ -16,22 +16,29 @@ if [ $? -ne 0 ]; then exit 1; fi
 
 VERSION=`ant -f build-swellrt.xml version | sed -n -e 's/^.*Version=//p'` 
 
-echo "Current version is $VERSION"
-
-echo "Need tagging?"
+echo "  current version is $VERSION Need docker tagging?"
 
 docker inspect $NAME:$VERSION > /dev/null 2>&1
 
 
 if [ $? -eq 1 ]
 then
-  echo "  there is not an image with this version"
-  echo "  tagging latest image"
-  docker tag $NAME $NAME:$VERSION
-  echo "  pushing tag"
-  docker push $NAME:$VERSION
+  echo "  Mmm, there is not a docker image with this version. Is it already git-tagged?"
+    git show v$VERSION > /dev/null 2>&1
 
-  if [ $? -ne 0 ]; then exit 1; fi
+    if [ $? -eq 0 ]
+    then
+      echo "  Yeah! git tag present!"
+      echo "Tagging latest image.."
+      docker tag $NAME $NAME:$VERSION
+      echo "Pushing tag.."
+      docker push $NAME:$VERSION
+
+      if [ $? -ne 0 ]; then exit 1; fi
+      echo "  done!"
+    else
+      echo "  git tag does not exist yet!"
+    fi
 else
-  echo "  tag already exists"
+  echo "  docker tag already exists!"
 fi
