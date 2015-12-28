9cadac52de18ea37028539d29bc006eac6e854cf
Pablo Ojanguren <pablojan@gmail.com>
2015-12-28 11:50:42 +0100

    Fixes #90 Js Base64 decoder crashes on very large messages (Chromium)

--- src/org/waveprotocol/box/webclient/client/atmosphere/WaveSocketAtmosphereCallback.java
+++ src/org/waveprotocol/box/webclient/client/atmosphere/WaveSocketAtmosphereCallback.java
@@ -5,8 +5,6 @@ import org.waveprotocol.box.webclient.client.WaveSocket.WaveSocketCallback;
 import org.waveprotocol.wave.client.events.ClientEvents;
 import org.waveprotocol.wave.client.events.NetworkStatusEvent;
 import org.waveprotocol.wave.client.events.NetworkStatusEvent.ConnectionStatus;
-import org.waveprotocol.wave.model.util.Base64DecoderException;
-import org.waveprotocol.wave.model.util.CharBase64;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,16 +13,16 @@ import java.util.logging.Logger;
 /**
  * A WaveSocketCallback implementations adding specific Atmosphere WebSocket
  * subprotocol features to the original Wave WebSocket subprotocol.
- * 
- * 
+ *
+ *
  * This implementation expects following atmosphere features enabled:
  * <ul>
  * <li>Heart beat messages</li>
  * <li>Track message size + Base64 message encoding</li>
  * </ul>
- * 
+ *
  * @author pablojan@gmail.com (Pablo Ojanguren)
- * 
+ *
  */
 public class WaveSocketAtmosphereCallback implements WaveSocket.WaveSocketCallback {
 
@@ -76,6 +74,17 @@ public class WaveSocketAtmosphereCallback implements WaveSocket.WaveSocketCallba
     delegate.onDisconnect();
   }
 
+  /**
+   * Decode Base64 string using browser native functions to avoid
+   * issues on large encoded strings.
+   *
+   * @param m
+   * @return
+   */
+  protected native String decodeBase64(String m) /*-{
+    return $wnd.atob(m);
+  }-*/;
+
 
   @Override
   public void onMessage(String message) {
@@ -86,7 +95,7 @@ public class WaveSocketAtmosphereCallback implements WaveSocket.WaveSocketCallba
       // feauture
       // NOTE: no Charset is specified, so this relies on UTF-8 as default
       // charset
-      String decoded = new String(CharBase64.decode(message));
+      String decoded = decodeBase64(message);
 
 
       // Ignore heart-beat messages
@@ -105,11 +114,12 @@ public class WaveSocketAtmosphereCallback implements WaveSocket.WaveSocketCallba
         delegate.onMessage(decoded);
       }
 
-    } catch (Base64DecoderException e) {
+    } catch (Exception e) {
       Log.severe(e.getMessage());
       // Errors here should passed to WaveWebSocket, instead of relaying on
       // client events.
-      ClientEvents.get().fireEvent(new NetworkStatusEvent(ConnectionStatus.PROTOCOL_ERROR));
+      ClientEvents.get().fireEvent(
+          new NetworkStatusEvent(ConnectionStatus.PROTOCOL_ERROR, "DECODE_EXCEPTION"));
     }
 
   }
