90365244eceb0cf239e90fd86993eb219fa546d1
Pablo Ojanguren <pablojan@gmail.com>
2016-05-17 17:36:02 +0200

    Add js launcher for new Web API

---
+++ war/swellrt-webapi.js
@@ -0,0 +1,30 @@
+
+// SwellRT bootstrap script
+
+// A fake SwellRT object to register on ready handlers
+// before the GWT module is loaded
+
+
+window.SwellRT = {
+
+  _readyHandlers: [],
+
+  ready: function(handler) {
+       if (!handler || typeof handler !== "function")
+         return;
+
+       this._readyHandlers.push(handler);
+      }
+}
+
+
+var scripts = document.getElementsByTagName('script');
+var thisScript = scripts[scripts.length -1];
+
+if (thisScript) {
+  var p = document.createElement('a');
+  p.href = thisScript.src;
+  var scriptSrc = p.protocol + "//" +p.host  + "/webapi/webapi.nocache.js";
+  document.write("<script src='"+scriptSrc+"'></script>");
+}
+
