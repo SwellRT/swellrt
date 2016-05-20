731d406601e62bd16f95c158476fd2bb2dc5e26d
Pablo Ojanguren <pablojan@gmail.com>
2016-05-20 12:24:01 +0200

    Added polyfill for proxy and reflect ES6

--- war/swellrt-webapi.js
+++ war/swellrt-webapi.js
@@ -17,13 +17,24 @@ window.SwellRT = {
       }
 }
 
-
 var scripts = document.getElementsByTagName('script');
 var thisScript = scripts[scripts.length -1];
+
+
+
+
 
 if (thisScript) {
   var p = document.createElement('a');
   p.href = thisScript.src;
+
+  // Polyfill for ES6 proxy/reflect
+  if (!window.Proxy || !window.Reflect) {
+    var reflectSrc = p.protocol + "//" +p.host  + "/reflect.js";
+    document.write("<script src='"+reflectSrc+"'></script>");
+  }
+
+
   var scriptSrc = p.protocol + "//" +p.host  + "/webapi/webapi.nocache.js";
   document.write("<script src='"+scriptSrc+"'></script>");
 }
