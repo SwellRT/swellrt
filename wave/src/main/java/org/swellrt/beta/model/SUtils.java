33cb66e0304e7504bf3303a467387e71c4cbf539
Pablo Ojanguren <pablojan@gmail.com>
2017-01-10 10:09:29 +0100

    Expose object's controller from js native object's view

--- wave/src/main/java/org/swellrt/beta/model/SUtils.java
+++ wave/src/main/java/org/swellrt/beta/model/SUtils.java
@@ -1,5 +1,8 @@
 package org.swellrt.beta.model;
 
+import org.swellrt.beta.model.remote.SNodeRemoteContainer;
+import org.waveprotocol.wave.client.common.util.JsoView;
+
 import com.google.gwt.core.client.JavaScriptObject;
 
 public class SUtils {
@@ -25,5 +28,35 @@ public class SUtils {
   
     throw new IllegalCastException("Error casting to primitive SNode");
   }
+  
+  /**
+   * Introspect a generic object (java or javascript)
+   * looking up a SNodeRemoteContainer.
+   * <p>
+   * 
+   * @param object
+   * @return
+   */
+  public static SNodeRemoteContainer asContainer(Object object) {
+    if (object == null)
+      return null;
+        
+    SNodeRemoteContainer node = null;
+    
+    if (object instanceof JavaScriptObject) {      
+      JsoView jso = JsoView.as((JavaScriptObject) object);
+      Object targetObject = jso.getObject("__target__");      
+      if (targetObject != null) {
+        if (targetObject instanceof SNodeRemoteContainer)
+          node = (SNodeRemoteContainer) targetObject;
+      }      
+    } else if (object instanceof SNodeRemoteContainer) {
+      node = (SNodeRemoteContainer) object;
+    }
+    
+    return node;
+  }
 
+
+  
 }
--- wave/src/main/java/org/swellrt/beta/model/js/SMapProxyHandler.java
+++ wave/src/main/java/org/swellrt/beta/model/js/SMapProxyHandler.java
@@ -2,7 +2,6 @@ package org.swellrt.beta.model.js;
 
 
 import org.swellrt.beta.common.SException;
-import org.swellrt.beta.model.IllegalCastException;
 import org.swellrt.beta.model.SMap;
 import org.swellrt.beta.model.SNode;
 import org.swellrt.beta.model.SUtils;
@@ -12,12 +11,20 @@ import jsinterop.annotations.JsType;
 
 @JsType(namespace = "swellrt")
 public class SMapProxyHandler extends ProxyHandler {
+  
+  private static final String PROP_TARGET = "__target__";
+  private static final String PROP_CONTROLLER = "__controller";
 
   @JsIgnore
   public SMapProxyHandler() {
   }
   
   public Object get(SMap target, String property, ProxyHandler reciever) throws SException {
+    
+    if (property.equals(PROP_TARGET) || property.equals(PROP_CONTROLLER)) {
+      return target;
+    }
+    
     Object node = target.get(property);
 
     if (node instanceof HasJsProxy) {
