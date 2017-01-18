d2d0c8eead52955f8f5a531df5d8a2b55d3d8198
Pablo Ojanguren <pablojan@gmail.com>
2017-01-18 13:35:11 +0100

    Work on read only primitive values

--- wave/src/main/java/org/swellrt/beta/model/SNode.java
+++ wave/src/main/java/org/swellrt/beta/model/SNode.java
@@ -1,8 +1,30 @@
 package org.swellrt.beta.model;
 
+import jsinterop.annotations.JsOptional;
 import jsinterop.annotations.JsType;
 
 @JsType(namespace = "swellrt", name = "Node")
 public interface SNode {
+  
+  public static SNode readOnly(Object object) throws IllegalCastException {
+    SNode node = SUtils.castToSNode(object);
+    return new SSuplementNode(node, true, false);
+  }
+  
+  public static SNode unique(@JsOptional Object object) throws IllegalCastException {
+    SNode node = null;
     
+    if (object != null) {
+      node = SUtils.castToSNode(object);
+      if (!(node instanceof SPrimitive)) {
+        node = null;
+      }
+    } 
+
+    if (node == null)
+      node = new SPrimitive(0);
+      
+    return new SSuplementNode(node, true, true);
+  }
+  
 }
