10981ebaf2341b66cad281865a86a31d1a681c37
Pablo Ojanguren <pablojan@gmail.com>
2017-03-04 12:18:11 +0100

    Disable service update from travis ci because long wait time

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -25,10 +25,10 @@ fi
 #
 # Reload services
 #
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-   echo "Preparing to update services..."
-   openssl aes-256-cbc -K $encrypted_dd5995a51c0a_key -iv $encrypted_dd5995a51c0a_iv -in .travis/secrets.tar.enc -out .travis/secrets.tar -d
-   tar xvf .travis/secrets.tar --directory .travis 
-   chmod ugo+x .travis/update_services.sh
-   .travis/update_services.sh	
-fi
+#if [ "$TRAVIS_BRANCH" == "master" ]; then
+#   echo "Preparing to update services..."
+#   openssl aes-256-cbc -K $encrypted_dd5995a51c0a_key -iv $encrypted_dd5995a51c0a_iv -in .travis/secrets.tar.enc -out .travis/secrets.tar -d
+#   tar xvf .travis/secrets.tar --directory .travis 
+#   chmod ugo+x .travis/update_services.sh
+#   .travis/update_services.sh	
+#fi
