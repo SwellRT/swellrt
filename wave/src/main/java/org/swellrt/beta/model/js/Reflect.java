9872e4b39e08bafaeeaf63c27355d0a380f9ec68
Pablo Ojanguren <pablojan@gmail.com>
2017-04-03 09:54:58 +0200

    Declare more methods in Reflect wrapper class

--- wave/src/main/java/org/swellrt/beta/model/js/Reflect.java
+++ wave/src/main/java/org/swellrt/beta/model/js/Reflect.java
@@ -6,6 +6,9 @@ import jsinterop.annotations.JsType;
 @JsType(isNative = true, namespace = JsPackage.GLOBAL)
 public class Reflect {
   
+  public static native boolean set(Object target, String key, Object value);
+  public static native boolean has(Object target, String key);
+  public static native boolean defineProperty(Object target, String key, Object attributes);
   public static native Object getOwnPropertyDescriptor(Object target, String key);
 
 }
