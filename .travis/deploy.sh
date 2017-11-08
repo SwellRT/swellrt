b8c05e45ce17c1c8f5a30ee000363f7bd8bea0ef
Pablo Ojanguren <pablojan@gmail.com>
2017-11-08 09:42:32 +0100

    Travis to deploy tagged docker images

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,27 +11,35 @@ set -e
 # Docker
 #
 
+DOCKER_IMAGE="p2pvalue/swellrt"
+DOCKER_TAG=""
 VERSION=`basename wave/build/libs/swellrt-*.jar | sed 's/^swellrt-//' | sed 's/.jar//'` 
+
 mv wave/build/libs/swellrt-$VERSION.jar wave/build/libs/swellrt.jar
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-   
-   echo "Creating docker image..."
-   docker build -t p2pvalue/swellrt .
-   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-   echo "Deploying docker image :latest"
-   docker push p2pvalue/swellrt
+if [ -n $TRAVIS_TAG ]; then
 
-elif [ -n $TRAVIS_TAG  ]; then
-
-  if [ "$TRAVIS_TAG" == "$VERSION" ]; then
-
-	echo "Ready to tag docker image $VERSION"
-
+  if [ "$TRAVIS_TAG" == "$VERSION"]; then
+	echo "A Git tag has been pushed. Ready to tag docker image as "$VERSION
+	DOCKER_TAG="-t "$DOCKER_IMAGE":"$VERSION
   else
-	echo "Version in build.gradle doesn't match Git Tag version"
-  fi
-  	  	
+	echo "A Git tag has been pushed, but it doesn't match generated Jar version"
+	echo "Did you tag the right commit? Did you forget to update build.gradle?"
+	echo ""
+	echo "Build aborted!!!" 
 	
+	exit -1
+  fi	
 fi
 
+
+
+if [ "$TRAVIS_BRANCH" == "master" || -n $TRAVIS_TAG ]; then
+   
+   echo "Creating Docker image..."
+   docker build -t $DOCKER_IMAGE":latest" $DOCKER_TAG .
+   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+   echo "Deploying Docker image"
+   docker push $DOCKER_IMAGE 
+
+fi
