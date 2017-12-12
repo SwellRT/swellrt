0d810bdafe5ff9447cb1db1cbcd9a886dc858919
Pablo Ojanguren <pablojan@gmail.com>
2017-12-12 09:58:49 +0100

    Java client now works exposing service frontend

--- wave/src/main/java/org/swellrt/beta/client/platform/java/Client.java
+++ wave/src/main/java/org/swellrt/beta/client/platform/java/Client.java
@@ -3,11 +3,17 @@ package org.swellrt.beta.client.platform.java;
 import java.util.Random;
 import java.util.Set;
 
+import org.swellrt.beta.client.DefaultFrontend;
 import org.swellrt.beta.client.ServiceConfig;
 import org.swellrt.beta.client.ServiceContext;
+import org.swellrt.beta.client.ServiceFrontend;
+import org.swellrt.beta.client.ServiceLogger;
 import org.swellrt.beta.client.SessionManager;
+import org.swellrt.beta.client.rest.JsonParser;
+import org.swellrt.beta.client.rest.ServiceOperation;
 import org.swellrt.beta.client.rest.operations.params.Account;
-import org.swellrt.beta.client.rest.operations.params.AccountImpl;
+import org.swellrt.beta.client.rest.operations.params.CredentialImpl;
+import org.swellrt.beta.client.rest.operations.params.ObjectIdImpl;
 import org.swellrt.beta.client.wave.Log;
 import org.swellrt.beta.client.wave.RemoteViewServiceMultiplexer;
 import org.swellrt.beta.client.wave.StagedWaveLoader;
@@ -15,7 +21,9 @@ import org.swellrt.beta.client.wave.WaveFactories;
 import org.swellrt.beta.client.wave.WaveLoader;
 import org.swellrt.beta.client.wave.ws.WebSocket;
 import org.swellrt.beta.common.ModelFactory;
-import org.swellrt.beta.model.wave.mutable.SWaveObject;
+import org.swellrt.beta.common.SException;
+import org.swellrt.beta.model.SObject;
+import org.swellrt.beta.model.SPrimitive;
 import org.waveprotocol.wave.client.wave.DiffData.WaveletDiffData;
 import org.waveprotocol.wave.client.wave.DiffProvider;
 import org.waveprotocol.wave.concurrencycontrol.common.TurbulenceListener;
@@ -26,22 +34,35 @@ import org.waveprotocol.wave.model.id.WaveletId;
 import org.waveprotocol.wave.model.version.HashedVersion;
 import org.waveprotocol.wave.model.wave.ParticipantId;
 
-import com.google.common.util.concurrent.FutureCallback;
+import com.google.gson.Gson;
 import com.google.gwt.core.client.Callback;
 
 public class Client {
 
+  public static ServiceFrontend get(String serverAddress) {
+
+    Client c = new Client(serverAddress);
+    c.start();
+    return c.getService();
+
+  }
+
   private final String serverAddress;
 
   ServiceContext context;
+  ServiceFrontend service;
 
-  public Client(String serverAddress) {
+  private ServiceFrontend getService() {
+    return service;
+  }
+
+  private Client(String serverAddress) {
     this.serverAddress = serverAddress;
   }
 
-  public void connect() {
+  private void start() {
 
-    ModelFactory.instance = null; // TODO to be completed
+    ModelFactory.instance = new JavaModelFactory(); // TODO to be completed
     ServiceConfig.configProvider = null; // TODO to be completed
     SessionManager sessionMgr = new SessionManager() {
 
@@ -126,6 +147,25 @@ public class Client {
     WaveFactories.mediumPriorityTimer = timerService;
     WaveFactories.highPriorityTimer = timerService;
 
+    WaveFactories.json = new JsonParser() {
+
+      Gson gson = new Gson();
+
+      @Override
+      public <R, S extends R> R parse(String json, Class<S> implType) {
+        R result = gson.fromJson(json, implType);
+        return result;
+      }
+
+      @Override
+      public <O> String serialize(O data) {
+        if (data != null)
+          return gson.toJson(data);
+        return null;
+      }
+
+    };
+
     context = new ServiceContext(sessionMgr, serverAddress, new DiffProvider.Factory() {
 
       @Override
@@ -142,34 +182,76 @@ public class Client {
       }
     });
 
-    Account accountData = new AccountImpl("fake@local.net");
+    service = new DefaultFrontend(context, new JavaServerOperationExecutor(context),
+        new ServiceLogger() {
 
-    context.init(accountData);
+          @Override
+          public void log(String message) {
+            System.out.println(message);
+          }
 
-    context.getObject(WaveId.of("local.net", "object"), new FutureCallback<SWaveObject>() {
+        });
 
-      @Override
-      public void onSuccess(SWaveObject result) {
-        System.out.println("Object loaded");
-      }
-
-      @Override
-      public void onFailure(Throwable t) {
-        System.err.println("Error " + t.getMessage());
-      }
-
-    });
+    /*
+     * context.getObject(WaveId.of("local.net", "object"), new
+     * FutureCallback<SWaveObject>() {
+     *
+     * @Override public void onSuccess(SWaveObject result) {
+     * System.out.println("Object loaded"); }
+     *
+     * @Override public void onFailure(Throwable t) {
+     * System.err.println("Error " + t.getMessage()); }
+     *
+     * });
+     *
+     */
 
   }
 
-  public void disconnect() {
+  public void clear() {
 
   }
 
   public static void main(String[] args) {
 
-    Client client = new Client("http://localhost:9898");
-    client.connect();
+    ServiceFrontend service = Client.get("http://localhost:9898");
+
+    service.login(new CredentialImpl("tam@local.net", "tam", true),
+        new ServiceOperation.Callback<Account>() {
+
+          @Override
+          public void onError(SException exception) {
+            System.err.println(exception.getMessage());
+
+          }
+
+          @Override
+          public void onSuccess(Account response) {
+            System.out.println("Login success " + response.getId());
+
+            service.open(new ObjectIdImpl("local.net/form-demo-default"),
+                new ServiceOperation.Callback<SObject>() {
+
+                  @Override
+                  public void onError(SException exception) {
+                    System.err.println("Error open " + exception.getMessage());
+                  }
+
+                  @Override
+                  public void onSuccess(SObject response) {
+                    try {
+                      System.out.println(
+                          "Object loaded -> " + SPrimitive.asString(response.node("form.address")));
+                    } catch (SException e) {
+                      e.printStackTrace();
+                    }
+
+                  }
+
+                });
+
+          }
+        });
 
   }
 
