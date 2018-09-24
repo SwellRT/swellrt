fd96707e1ca654ff6c498707a21def31054cd0a7
Pablo Ojanguren <pablojan@gmail.com>
2018-09-24 19:14:36 +0200

    Comments about transient wavelets and federation

--- wave/src/main/java/org/waveprotocol/box/server/waveserver/DeltaStoreTransient.java
+++ wave/src/main/java/org/waveprotocol/box/server/waveserver/DeltaStoreTransient.java
@@ -1,7 +1,14 @@
 package org.waveprotocol.box.server.waveserver;
 
 /**
- * Delta stores meant to use only in memory data structures.
+ * A delta store suitable to store transient wavelets. Transient wavelets must
+ * store no relevant information for applications. <br>
+ * <p>
+ * WARNING! Deletion of transient wavelets is only safe between server reboots
+ * and when the wavelet is not shared across domains (federated).
+ * <p>
+ * For federated wavelets, a coordinated deletion protocol should be put in
+ * place.
  *
  * @author pablojan@gmail.com (Pablo Ojanguren)
  *
